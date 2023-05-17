339aba01861d7ddc0124a40dd17ca0d16a389af6
Jisan-mia <jisanmia47@gmail.com>
2023-05-17 23:51:35 +0600

    jest testing: tested codes using describe, test and with matchers

--- script.test.js
+++ script.test.js
@@ -1,11 +1,25 @@
-const {sum} = require('./script.js')
-
+const { sum, getLargest } = require("./script.js");
 
 // test function 1st parameter is a string which the name of our test
 // 2nd parameter is a function, inside the function every code is our test
 
-test('it adds 2 numbers correctly', () => {
-  const a = 2
-  const b = 3
-  expect(sum(a,b)).toBe(5)
-})
+describe("#sum", () => {
+  test("it adds 2 numbers correctly", () => {
+    const a = 2;
+    const b = 3;
+    expect(sum(a, b)).toBe(5);
+  });
+});
+
+describe("#getLargest", () => {
+  test("it returns largest number from the array", () => {
+    const arr = [1, 2, 3, 4, 5];
+    expect(getLargest(arr)).toBe(5);
+  });
+
+  describe('with an empty array', () => {
+    test("It returns null when given and empty array to getLargest func", () => {
+      expect(getLargest([])).toBeNull();
+    });
+  });
+});
